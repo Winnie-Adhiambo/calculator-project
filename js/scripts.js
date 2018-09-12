d37bf93d1329c1e9fd2e137f5be06b19f292fe02
MoringaSchool <winnieadhiambo600@gmail.com>
2018-09-12 12:41:27 +0300

    add the link in html file

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var add = function (number1 , number2){
+return number1 + number2;
+};
+var number1 = parseInt(prompt("enter a value"));
+var number2 = parseInt(prompt("enter a value"));
+var result = add (number1 , number2)
+alert (result)
+
+var substract = function (number1 , number2){
+return number1 - number2;
+}
+var number1 = parseInt(prompt("enter a value"));
+var number2 = parseInt(prompt("enter a value"));
+var result = substract (number1 , number2)
+alert (result)
+
+var multiply = function (number1 , number2){
+return number1 * number2
+}
+var number1 = parseInt(prompt("enter a value"));
+var number2 = parseInt(prompt("enter a value"));
+var result = multiply (number1 , number2)
+alert (result)
+
+var divide = function (number1 , number2){
+return number1 / number2
+};
+var number1 = parseInt(prompt("enter a value"));
+var number2 = parseInt(prompt("enter a value"));
+var result = divide(number1 , number2)
+alert (result)
